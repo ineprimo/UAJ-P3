b392eb5739e971d668c43d620f24d5d333b36a07
Jimbo <javicentear@gmail.com>
2026-04-16 15:34:31 +0200

    Heatmap
    (Creo) que aun tiene problemas

--- Hey ladies touch the ground/deserializer.py
+++ Hey ladies touch the ground/deserializer.py
@@ -1,10 +1,13 @@
 import json
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 folder_path = "./"
 
 def gameSession(event_list):
     sessionMatchesTimes = []
+    clicks = []
 
     for currentEvent in event_list:
         if currentEvent["eventType"] == "match_start":
@@ -12,8 +15,40 @@ def gameSession(event_list):
         if currentEvent["eventType"] == "match_end":
             tsMatchEnd = currentEvent["timestamp"]
             sessionMatchesTimes.append(tsMatchEnd - tsMatchStart)
+        if currentEvent["eventType"] == "mouse_click":
+            clicks.append((currentEvent["x"], currentEvent["y"]))
 
-    return sessionMatchesTimes
+
+    return sessionMatchesTimes, clicks
+
+def processTimes(session_times, session_number):
+    i = 0
+    for matchTime in session_times:
+        totalSeconds = matchTime / 1000
+        minutes = int(totalSeconds / 60)
+        remainingSeconds = totalSeconds % 60
+        print(f"Session {session_number} match {i} time: {minutes}:{remainingSeconds} minutes")
+
+        i += 1
+
+#TODO: que se haga por cada match y no por sesion?
+def processClicks(session_clicks, session_number):
+    heatmap = pd.DataFrame(session_clicks, columns=["x", "y"])
+
+    if not heatmap.empty:
+        plt.figure(figsize=(10, 6))
+        
+        bg_path = "background.png" 
+        if os.path.exists(bg_path):
+            img = plt.imread(bg_path)
+            height, width = img.shape[:2]
+            plt.imshow(img, extent=[0, width, 0, height])
+
+        plt.hist2d(heatmap["x"], heatmap["y"], bins=50, cmap="hot", alpha=0.6, 
+                    range=[[0, width], [0, height]], cmin=1)
+        plt.colorbar(label="Densidad de clicks")
+        plt.title(f"Mapa de Calor de Clicks - Sesión {session_number}")
+        plt.show()
 
 ### Main section ###
 if __name__ == "__main__":
@@ -30,15 +65,11 @@ if __name__ == "__main__":
                 for line in f:
                     data.append(json.loads(line))
 
-            sessionMatchesTimes = gameSession(data)
-            i = 0
-            for matchTime in sessionMatchesTimes:
-                totalSeconds = matchTime / 1000
-                minutes = int(totalSeconds / 60)
-                remainingSeconds = totalSeconds % 60
-                print("Session " + str(s) + " match " + str(i) + " time: " + str(minutes) + ":" + str(remainingSeconds) + " minutes")
+            sessionMatchesTimes, clicks = gameSession(data)
 
-                i += 1
+            processTimes(sessionMatchesTimes, s)
+            processClicks(clicks, s)
+
             s += 1
 
 
