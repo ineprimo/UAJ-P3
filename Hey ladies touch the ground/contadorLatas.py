ef23f57641d27aa5681ed6546e28e9024f3fabbb
Vivu24 <javtirad@ucm.es>
2026-04-20 21:15:29 +0200

    Metrica promedio

--- Hey ladies touch the ground/contadorLatas.py
+++ Hey ladies touch the ground/contadorLatas.py
@@ -39,6 +39,60 @@ def calculate_m1():
                 
     print("\n====================\n\n")
 
+
+def calculate_m2():
+    print("\n====================\n")
+    print("\nMETRICA DE TASA DE ACIERTOS / FALLOS\n")
+    
+    archivos_validos = 0
+    
+    # Buscamos Jsons
+    for filename in os.listdir(folder_path):
+        if filename.startswith("telemetria_sesion_") and filename.endswith(".json"):
+            archivos_validos += 1
+            file_path = os.path.join(folder_path, filename)
+            
+            with open(file_path, "r") as f:
+                events = [json.loads(line) for line in f]
+            
+            # Guardamos los aciertos y fallos
+            rendimiento_por_partida = {}
+            
+            for event in events:
+                if "matchId" in event:
+                    match_id = event["matchId"]
+                    session_id = event["sessionId"]
+                    clave_partida = f"Sesión {session_id} / partida {match_id}"
+                    
+                    if clave_partida not in rendimiento_por_partida:
+                        rendimiento_por_partida[clave_partida] = {"aciertos": 0, "fallos": 0}
+                        
+                    # Comprobamos el rendimiento
+                    if event["eventType"] == "can_landed":
+                        color_lata = event.get("canType")
+                        destino = event.get("targetType")
+                        
+                        if color_lata == destino:
+                            rendimiento_por_partida[clave_partida]["aciertos"] += 1
+                        else:
+                            rendimiento_por_partida[clave_partida]["fallos"] += 1
+            
+            print(f"\nArchivo analizado: {filename}\n")
+            for partida, datos in rendimiento_por_partida.items():
+                aciertos = datos["aciertos"]
+                fallos = datos["fallos"]
+                total_lanzamientos = aciertos + fallos
+                
+                if total_lanzamientos > 0:
+                    tasa = (aciertos / total_lanzamientos) * 100
+                    print(f" - {partida} : Tasa de acierto del {tasa:.2f}% (Aciertos: {aciertos}, Fallos: {fallos})")
+                else:
+                    print(f" - {partida} : 0 latas lanzadas")
+                
+    print("\n====================\n\n")
+
+
 ### Main section ###
 if __name__ == "__main__":
     calculate_m1()
+    calculate_m2()
